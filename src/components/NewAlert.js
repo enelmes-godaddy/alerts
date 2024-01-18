aa35550a29bf1d32d9ea750a3f2a619330a7640c
Elena Nelmes <enelmes@godaddy.com>
2024-01-18 17:00:06 -0500

    Use invisible text elements to ensure baseline alignment of icon, text and CTA

--- src/components/NewAlert.js
+++ src/components/NewAlert.js
@@ -12,27 +12,26 @@ function NewAlert(props) {
 
     return (
         <div
-            // style={{ width: alertWidth + "px" }}
             style={{ width: alertWidthValue }}
             className={classNames("NewAlert", emphasis)}
         >
             <div className="Alert--content">
-                <div className="Alert--icon-and-text-wrapper">
-                    <div className="Alert--icon">
-                        {ALERT_ICONS[emphasis]}
-                    </div>
-                    <div className="Alert--text-lockup">
+                <div className="Alert--icon">
+                    {ALERT_ICONS[emphasis]}
+                </div>
+                <div className="Alert--text-and-cta-wrapper">
+                    <div className="Alert--text">
                         {header && <div className="Alert--header">
-                            <h3 className="ux-text ux-text-size1 mb-s">{header}</h3>
+                            <h3 className="ux-text ux-text-size1">{header}</h3>
                         </div>}
                         {body && <div className="Alert--body">
                             <p className="ux-text ux-text-paragraph ux-text-size0">{body}</p>
                         </div>}
                     </div>
-                </div>
-                {cta && <div className={classNames("Alert--action", { "wrap-cta-label": ctaWrap })}>
+                {cta && <div className={classNames("Alert--cta", { "wrap-cta-label": ctaWrap })}>
                     <Button design="secondary" size="small" text={cta} />
                 </div>}
+                </div>
             </div>
             {isDismissable && <button className="Alert--dismiss">{DISMISS}</button>}
         </div >
